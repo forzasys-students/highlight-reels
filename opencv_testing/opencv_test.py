6113bc16942109ae7796eb541a15560ea729bdbc
Leon Tongfai Lam <s352005@oslomet.no>
2024-05-05 22:05:05 +0200

    Changes in opencv_test.py
    paste image not working

--- opencv_testing/opencv_test.py
+++ opencv_testing/opencv_test.py
@@ -56,7 +56,7 @@ def rounded_rectangle(src, top_left, bottom_right, color, radius=1, thickness=1,
     cv2.addWeighted(overlay, opacity, src, 1 - opacity, 0, src)
     return src
 
-def generate_rect(width, height, x_offset, y_offset, end_x=1, end_y=1, color=(255, 255, 255), opacity=0.11):
+def generate_rect(x_offset, y_offset, end_x=1, end_y=1, color=(255, 255, 255), opacity=0.11, width=1920, height=1080):
     # If rectangle is centered both horiz. and vert.
     if (end_x == 1 and end_y == 1):
         top_left = (int(width * x_offset), int(height * y_offset))
@@ -72,16 +72,25 @@ def generate_rect(width, height, x_offset, y_offset, end_x=1, end_y=1, color=(25
 
     rounded_rectangle(frame, top_left, bottom_right, color, radius=0.1, thickness=-1, opacity=opacity)
     
-def generate_center_text(frame, text, font_size, width, height, x_offset, y_offset, end_x=1, end_y=1, color=(0,0,0), thickness=1):
+def generate_center_text(frame, text, font_size, x_offset, y_offset, end_x=1, end_y=1, position=0, color=(0,0,0), thickness=1, width=1920, height=1080):
     if (end_x == 1 and end_y == 1):
         center_w = int(width / 2)
         center_h = int(height / 2)
+        if position != 0:
+            w_offset = int((width - 2*(x_offset*width))*position)
+            center_w = center_w - w_offset
     elif (end_x != 1 and end_y != 1):
         center_w = int(width*x_offset + ((width*end_x - width*x_offset)/2))
         center_h = int(height*y_offset + ((height*end_y - height*y_offset)/2))
+        if position != 0:
+            w_offset = int((width*end_x - width*x_offset)*position)
+            center_w = center_w - w_offset
     elif (end_y != 1):
         center_w = int(width / 2)
         center_h = int(height*y_offset + ((height*end_y - height*y_offset)/2))
+        if position != 0:
+            w_offset = int((width - 2*(x_offset*width))*position)
+            center_w = center_w - w_offset
     
     font = cv2.FONT_HERSHEY_DUPLEX
     text_size, _ = cv2.getTextSize(text, font, font_size, thickness)
@@ -89,8 +98,42 @@ def generate_center_text(frame, text, font_size, width, height, x_offset, y_offs
 
     cv2.putText(frame, text, (text_origin[0], text_origin[1]), font, font_size, color, thickness, cv2.LINE_AA)
 
+def is_image(var):
+    return isinstance(var, Image.Image)
 
-input_video_path = 'opencv_testing/video_16_9.mp4'
+def generate_center_logo(temp_image, logo, logo_w, logo_h, x_offset, y_offset, end_x=1, end_y=1, position=0, width=1920, height=1080):
+    if (end_x == 1 and end_y == 1):
+        center_w = int(width / 2)
+        center_h = int(height / 2)
+        if position != 0:
+            w_offset = int((width - 2*(x_offset*width))*position)
+            center_w = center_w - w_offset
+    elif (end_x != 1 and end_y != 1):
+        center_w = int(width*x_offset + ((width*end_x - width*x_offset)/2))
+        center_h = int(height*y_offset + ((height*end_y - height*y_offset)/2))
+        if position != 0:
+            w_offset = int((width*end_x - width*x_offset)*position)
+            center_w = center_w - w_offset
+    elif (end_y != 1):
+        center_w = int(width / 2)
+        center_h = int(height*y_offset + ((height*end_y - height*y_offset)/2))
+        if position != 0:
+            w_offset = int((width - 2*(x_offset*width))*position)
+            center_w = center_w - w_offset
+
+    if not is_image(logo):
+        image = Image.open(f"resources/img/{logo}")
+        
+
+    new_size = (logo_w, logo_h)
+
+    resized_image = image.resize(new_size)
+
+    logo_origin = (int(center_w - new_size[0] / 2), int(center_h + new_size[1] / 2))
+
+    temp_image.paste(resized_image, (logo_origin[0], logo_origin[1]))
+
+input_video_path = 'opencv_testing/video_2.mp4'
 output_video_path = 'opencv_testing/16_9_meta.mp4'
 
 if os.path.exists(output_video_path):
@@ -119,6 +162,7 @@ out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
 #page 1
 p1_big_x = 0.14
 p1_big_y = 0.4045
+p1_team_font = 1
 
 p1_small_x = 0.31
 p1_small_y = 0.33
@@ -135,6 +179,7 @@ p2_small_font = 1
 
 p2_big_y = 0.081
 p2_big_end_y = 0.185
+p2_big_font = 1.5
 
 player_y = 0.88
 player_end_y = 0.95
@@ -157,27 +202,45 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
+    image = Image.fromarray(frame)
     i += 1
+
+    temp_image = image.copy()
+    draw_temp = ImageDraw.Draw(temp_image)
+
     # Fade-in effect goalstamp & match-info
     if i < int(duration * 0.1):
-        generate_rect(width, height, p1_big_x, p1_big_y)
-        generate_rect(width, height, p1_small_x, p1_small_y, end_y=p1_small_end_y)
-        generate_center_text(frame, "ALLSVENSKAN", p1_small_font, width, height, p1_small_x, p1_small_y, end_y=p1_small_end_y)
+        # team vs team rect
+        generate_rect(p1_big_x, p1_big_y)
+        generate_center_text(frame, "Sparta Rotterdam FC", p1_team_font, p1_big_x, p1_big_y, position=0.2) # Positioned centered to the left of rect
+        generate_center_text(frame, "FC Volendam", p1_team_font, p1_big_x, p1_big_y, position=-0.2) # Positioned centered to the right of rect
+        generate_center_logo(temp_image, "allsvenskan.png", 500, 500, p1_big_x, p1_big_y)
+        # league rect
+        generate_rect(p1_small_x, p1_small_y, end_y=p1_small_end_y)
+        generate_center_text(frame, "ALLSVENSKAN", p1_small_font, p1_small_x, p1_small_y, end_y=p1_small_end_y)
+        
+        
         
     if i > int(duration* 0.08):
-        generate_rect(width, height, p2_x, p2_small_y, p2_end_x, p2_small_end_y)
-        generate_center_text(frame, "ALLSVENSKAN", p2_small_font, width, height, p2_x, p2_small_y, end_x=p2_end_x, end_y=p2_small_end_y)
-        generate_rect(width, height, p2_x, p2_big_y, p2_end_x, p2_big_end_y)
-
+        # p2
+        # small league rect (top left)
+        generate_rect(p2_x, p2_small_y, p2_end_x, p2_small_end_y)
+        generate_center_text(frame, "ALLSVENSKAN", p2_small_font, p2_x, p2_small_y, end_x=p2_end_x, end_y=p2_small_end_y)
+        # scoreboard
+        generate_rect(p2_x, p2_big_y, p2_end_x, p2_big_end_y)
+        generate_center_text(frame, "05:20", p2_big_font, p2_x, p2_big_y, p2_end_x, p2_big_end_y)
+        generate_center_text(frame, "1", p2_big_font, p2_x, p2_big_y, p2_end_x, p2_big_end_y, position=0.4)
+        generate_center_text(frame, "2", p2_big_font, p2_x, p2_big_y, p2_end_x, p2_big_end_y, position=-0.4)
+        generate_center_text(frame, "logo", p2_big_font, p2_x, p2_big_y, p2_end_x, p2_big_end_y, position=-0.25)
     if i > int(duration * 0.4) and i < (duration * 0.7):
-        generate_rect(width, height, p2_x, player_y, p2_end_x, player_end_y)
+        generate_rect(p2_x, player_y, p2_end_x, player_end_y)
 
     if i > (duration * 0.8):
-        generate_rect(width, height, p3_small_x, p3_small_y, end_y=p3_small_end_y)
-        generate_center_text(frame, "ALLSVENSKAN", p3_small_font, width, height, p3_small_x, p3_small_y, end_y=p3_small_end_y)
+        generate_rect(p3_small_x, p3_small_y, end_y=p3_small_end_y)
+        generate_center_text(frame, "ALLSVENSKAN", p3_small_font, p3_small_x, p3_small_y, end_y=p3_small_end_y)
 
-        generate_rect(width, height, p3_x, p3_big_end_y, end_y=p3_player_end_y)
-        generate_rect(width, height, p3_x, p3_big_y, end_y=p3_big_end_y, opacity=0)
+        generate_rect(p3_x, p3_big_end_y, end_y=p3_player_end_y)
+        generate_rect(p3_x, p3_big_y, end_y=p3_big_end_y, opacity=0)
     
     out.write(frame)
     
